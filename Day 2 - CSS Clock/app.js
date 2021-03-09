04cb2c2b6459ab37df6f2b4db4c13cd6243cdbf9
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2021-03-09 23:44:56 +0530

    Seconds Completed

--- Day 2 - CSS Clock/app.js
+++ Day 2 - CSS Clock/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const seconds = document.querySelector('.second-hand');
 
 function hands(){
@@ -14,21 +13,4 @@ function hands(){
 
 }
 
-=======
-const seconds = document.querySelector('.second-hand');
-
-function hands(){
-
-    const now = new Date(); 
-
-    const secondsValue = now.getSeconds();
-    const secondsDegree = ((secondsValue/60)*360);
-    
-    seconds.style.transform = `rotate(${secondsDegree}deg)`; 
-
-    console.log(secondsValue); 
-
-}
-
->>>>>>> 423e5f5c528848b0a66f935bd3bf4ae96c573309
 setInterval(hands, 1000); 
