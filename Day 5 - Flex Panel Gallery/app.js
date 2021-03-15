130ae31f616979b836f156ad020e0cf216108892
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2021-03-15 20:22:22 +0530

    Final Version - Refined Code

--- Day 5 - Flex Panel Gallery/app.js
+++ Day 5 - Flex Panel Gallery/app.js
@@ -3,18 +3,17 @@ const flex = document.querySelectorAll('.flexbox .flexitem');
 flex.forEach(item => item.addEventListener('mouseenter', enter)); 
 
 function enter(){
-    var element = document.getElementById(this.id); 
-    element.classList.toggle('onmouse'); 
-
-    console.log('Entered: ', this.id); 
+    this.classList.add('onmouse'); 
+    this.classList.remove('leaveClass'); 
+    console.table('Entered: ', this.id); 
 }
 
 flex.forEach(item => item.addEventListener('mouseleave', leave)); 
 
 function leave(){
-    var element = document.getElementById(this.id); 
-    element.classList.toggle('leaveClass'); 
-    console.log('Left: ',this.id); 
+    this.classList.remove('onmouse'); 
+    this.classList.toggle('leaveClass');
+    console.table('Left: ',this.id); 
 }
 
  
